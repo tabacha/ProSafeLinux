5ed09420800909730143c04dc2d88833105c654d
Sven Anders <github-commit@sven.anders.im>
2012-06-17 14:58:45 +0200

    extended check script

--- check-syntax.sh
+++ check-syntax.sh
@@ -1,4 +1,37 @@
-#!/bin/sh
-pylint -d C0111  -d W0511  -d W0613 -d R0201 -d W0223 -d W0702 -d R0902 psl_typ.py
+#!/bin/bash
+DOCSTRING=C0111
+FIXME=W0511
+UNUSED_ARG=W0613
+EXCEPTION_TYPE=W0702
+INSTANCE_ATTRIBUTE=R0902
+METHOD_FUNCTION=R0201
+ABSTRACT=W0223
+LOG=/tmp/psl_check_syntax.log
 
-pylint -d C0111  -d W0511  -d W0613    -d W0702 -d R0902 psl.py
+if [ -f $LOG ] ; then
+    echo Logfile $LOG exists, please remove first
+    exit 1
+fi
+
+pylint -d $DOCSTRING  -d $FIXME -d $UNUSED_ARG -d $EXCEPTION_TYPE \
+       -d $INSTANCE_ATTRIBUTE  -d $METHOD_FUNCTION \
+       -d $ABSTRACT psl_typ.py >$LOG 2>&1
+
+T=$?
+
+if [ "$T" != "0" ] ; then
+    cat $LOG
+fi
+
+pylint -d $DOCSTRING  -d $FIXME -d $UNUSED_ARG -d $EXCEPTION_TYPE \
+       -d $INSTANCE_ATTRIBUTE psl.py >$LOG 2>&1
+
+M=$?
+if [ "$M" != "0" ] ; then
+    cat $LOG
+fi
+
+rm $LOG
+if [ "$M" == "0" -a "$T" == "0" ] ; then
+    echo Okay
+fi
