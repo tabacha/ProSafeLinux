6bd50f36d926a3592b2e38f1d976c55b7af7a5d9
Sven Anders <github-commit@sven.anders.im>
2012-06-19 12:13:53 +0200

    Do anything to meet the styleguide

--- psl-cmd.py
+++ psl-cmd.py
@@ -5,78 +5,26 @@ import argparse
 import sys
 from psl import ProSafeLinux
 import psl_typ
-g = ProSafeLinux()
 
-
-parser = argparse.ArgumentParser(
-    description='Manage Netgear ProSafe Plus switches under linux.')
-parser.add_argument("--interface", nargs=1, help="Interface", default=["eth0"])
-parser.add_argument("--debug", help="Debug output", action='store_true')
-subparsers = parser.add_subparsers(help='operation', dest="operation")
-
-discover_parser = subparsers.add_parser('discover',
-    help='Find all switches in all subnets')
-
-query_parser = subparsers.add_parser("query",
-    help="Query values from the switch")
-query_parser.add_argument("--mac", nargs=1,
-    help="Hardware adresse of the switch", required=True)
-query_parser.add_argument("--passwd", nargs=1, help="password")
-ch = []
-for cmd in g.get_query_cmds():
-    ch.append(cmd.get_name())
-ch.append("all")
-query_parser.add_argument("query", nargs="+", help="What to query for",
-    choices=ch)
-
-query_parser = subparsers.add_parser("query_raw",
-    help="Query raw values from the switch")
-query_parser.add_argument("--mac", nargs=1,
-    help="Hardware adresse of the switch", required=True)
-query_parser.add_argument("--passwd", nargs=1,
-    help="password")
-
-
-set_parser = subparsers.add_parser("set", help="Set values to the switch")
-set_parser.add_argument("--mac", nargs=1,
-    help="Hardware adresse of the switch", required=True)
-set_parser.add_argument("--passwd", nargs=1, help="password", required=True)
-
-for cmd in g.get_setable_cmds():
-    if isinstance(cmd, psl_typ.PslTypAction):
-        set_parser.add_argument("--" + cmd.get_name(),
-            dest=cmd.get_name(), action='store_true')
-
-    else:
-        if isinstance(cmd, psl_typ.PslTypBoolean):
-            set_parser.add_argument("--" + cmd.get_name(), nargs=1,
-                choices=["on", "off"])
-        else:
-            set_parser.add_argument("--" + cmd.get_name(), nargs=1)
-
-args = parser.parse_args()
-interface = args.interface[0]
-
-g.bind(interface)
-
-
-def discover():
+def discover(args, switch):
+    "Search for Switches"
     print "Searching for ProSafe Plus Switches ...\n"
-    g.discover()
+    switch.discover()
 
 
-def set_switch():
+def set_switch(args, switch):
+    "Set values in one switch"
     cmds = {ProSafeLinux.CMD_PASSWORD: args.passwd[0]}
-    for cmd in g.get_setable_cmds():
-        if vars(args)[cmd.get_name()] is not None:
-            if isinstance(cmd, psl_typ.PslTypAction):
-                if vars(args)[cmd.get_name()]:
-                    cmds[cmd] = True
+    for scmd in switch.get_setable_cmds():
+        if vars(args)[scmd.get_name()] is not None:
+            if isinstance(scmd, psl_typ.PslTypAction):
+                if vars(args)[scmd.get_name()]:
+                    cmds[scmd] = True
             else:
-                if isinstance(cmd, psl_typ.PslTypBoolean):
-                    cmds[cmd] = (vars(args)[cmd.get_name()][0] == "on")
+                if isinstance(scmd, psl_typ.PslTypBoolean):
+                    cmds[scmd] = (vars(args)[scmd.get_name()][0] == "on")
                 else:
-                    cmds[cmd] = vars(args)[cmd.get_name()][0]
+                    cmds[scmd] = vars(args)[scmd.get_name()][0]
 
     if ProSafeLinux.CMD_DHCP in cmds:
         if cmds[ProSafeLinux.CMD_DHCP]:
@@ -99,75 +47,134 @@ def set_switch():
             return
 
     print "Changing Values..\n"
-    g.transmit(cmds, args.mac[0], g.transfunc)
+    switch.transmit(cmds, args.mac[0], switch.transfunc)
 
 
-def query():
+def query(args, switch):
+    "query values form the switch"
     print "Query Values..\n"
     if not(args.passwd == None):
-        login = {g.CMD_PASSWORD: args.passwd[0]}
-        g.transmit(login, args.mac[0], g.transfunc)
-    cmd = []
-    for q in args.query:
-        if q == "all":
-            for k in g.get_query_cmds():
+        login = {switch.CMD_PASSWORD: args.passwd[0]}
+        switch.transmit(login, args.mac[0], switch.transfunc)
+    query_cmd = []
+    for qarg in args.query:
+        if qarg == "all":
+            for k in switch.get_query_cmds():
                 if ((k != ProSafeLinux.CMD_VLAN_ID) and
                     (k != ProSafeLinux.CMD_VLAN802_ID)):
-                    cmd.append(k)
+                    query_cmd.append(k)
         else:
-            c = g.get_cmd_by_name(q)
-            cmd.append(c)
-    g.query(cmd, args.mac[0], g.storefunc)
-    for key in g.outdata.keys():
+            query_cmd.append(switch.get_cmd_by_name(qarg))
+    switch.query(query_cmd, args.mac[0], switch.storefunc)
+    for key in switch.outdata.keys():
         if isinstance(key, psl_typ.PslTyp):
-            key.print_result(g.outdata[key])
+            key.print_result(switch.outdata[key])
         else:
             if args.debug:
-                print "-%-29s%s" % (key, g. outdata[key])
+                print "-%-29s%s" % (key, switch. outdata[key])
 
 
-def query_raw():
+def query_raw(args, switch):
+    "get all values, even unkown"
     print "QUERY DEBUG RAW"
     if not(args.passwd == None):
-        login = {g.CMD_PASSWORD: args.passwd[0]}
-        g.transmit(login, args.mac[0], g.transfunc)
+        login = {switch.CMD_PASSWORD: args.passwd[0]}
+        switch.transmit(login, args.mac[0], switch.transfunc)
     i = 0x0001
     while (i < ProSafeLinux.CMD_END.get_id()):
-        cmd = []
-        cmd.append(psl_typ.PslTypHex(i, "Command %d" % i))
+        query_cmd = []
+        query_cmd.append(psl_typ.PslTypHex(i, "Command %d" % i))
         try:
-            g.query(cmd, args.mac[0], g.rec_raw)
+            switch.query(query_cmd, args.mac[0], switch.rec_raw)
             found = None
-            for c in g.outdata.keys():
-                if (isinstance(c, psl_typ.PslTyp)):
-                    if c.get_id() == i:
-                        found = c
+            for qcmd in switch.outdata.keys():
+                if (isinstance(qcmd, psl_typ.PslTyp)):
+                    if qcmd.get_id() == i:
+                        found = qcmd
 
             if found is None:
-                print "NON:%04x:%-29s:%s" % (i, "", g.outdata["raw"])
+                print "NON:%04x:%-29s:%s" % (i, "", switch.outdata["raw"])
             else:
-                print "RES:%04x:%-29s:%s " % (i, g.outdata[found],
-                    g.outdata["raw"])
+                print "RES:%04x:%-29s:%s " % (i, switch.outdata[found],
+                    switch.outdata["raw"])
             if args.debug:
-                for key in g.outdata.keys():
-                    print "%x-%-29s%s" % (i, key, g.outdata[key])
+                for key in switch.outdata.keys():
+                    print "%x-%-29s%s" % (i, key, switch.outdata[key])
         except (KeyboardInterrupt, SystemExit):
             raise
         except:
             print "ERR:%04x:%s" % (i, sys.exc_info()[1])
         i = i + 1
 
-cmdHash = {
-    "discover": discover,
-    "set": set_switch,
-    "query": query,
-    "query_raw": query_raw,
-}
+def main():
+    "main program"
+    cmd_funcs = {
+        "discover": discover,
+        "set": set_switch,
+        "query": query,
+        "query_raw": query_raw,
+    }
+    
+    switch = ProSafeLinux()
+    parser = argparse.ArgumentParser(
+        description='Manage Netgear ProSafe Plus switches under linux.')
+    parser.add_argument("--interface", nargs=1, help="Interface", 
+        default=["eth0"])
+    parser.add_argument("--debug", help="Debug output", action='store_true')
+    subparsers = parser.add_subparsers(help='operation', dest="operation")
+    
+    subparsers.add_parser('discover', help='Find all switches in all subnets')
+    
+    query_parser = subparsers.add_parser("query",
+        help="Query values from the switch")
+    query_parser.add_argument("--mac", nargs=1,
+        help="Hardware adresse of the switch", required=True)
+    query_parser.add_argument("--passwd", nargs=1, help="password")
+    choices = []
+    for cmd in switch.get_query_cmds():
+        choices.append(cmd.get_name())
+    choices.append("all")
+    query_parser.add_argument("query", nargs="+", help="What to query for",
+        choices=choices)
+    
+    query_parser = subparsers.add_parser("query_raw",
+        help="Query raw values from the switch")
+    query_parser.add_argument("--mac", nargs=1,
+        help="Hardware adresse of the switch", required=True)
+    query_parser.add_argument("--passwd", nargs=1,
+        help="password")
+    
+    
+    set_parser = subparsers.add_parser("set", help="Set values to the switch")
+    set_parser.add_argument("--mac", nargs=1,
+        help="Hardware adresse of the switch", required=True)
+    set_parser.add_argument("--passwd", nargs=1, help="password", required=True)
+    
+    for cmd in switch.get_setable_cmds():
+        if isinstance(cmd, psl_typ.PslTypAction):
+            set_parser.add_argument("--" + cmd.get_name(),
+                dest=cmd.get_name(), action='store_true')
+    
+        else:
+            if isinstance(cmd, psl_typ.PslTypBoolean):
+                set_parser.add_argument("--" + cmd.get_name(), nargs=1,
+                    choices=["on", "off"])
+            else:
+                set_parser.add_argument("--" + cmd.get_name(), nargs=1)
+    
+    args = parser.parse_args()
+    interface = args.interface[0]
+    
+    switch.bind(interface)
+    
+    
+    
+    if (args.debug):
+        switch.set_debug_output()
+    
+    if args.operation in cmd_funcs:
+        cmd_funcs[args.operation](args, switch)
+    else:
+        print "ERROR: operation not found!"
 
-if (args.debug):
-    g.set_debug_output()
-
-if args.operation in cmdHash:
-    cmdHash[args.operation]()
-else:
-    print "ERROR: operation not found!"
+main()
