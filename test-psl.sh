fd6c0fef0996c7ed72fb07d1565670ae9a790006
Sven Anders <github-commit@sven.anders.im>
2012-06-19 15:39:17 +0200

    Rename psl-cmd.py to psl.py

--- test-psl.sh
+++ test-psl.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 NAME=name$(date +%s)
-./psl-cmd.py --interface $INTERFACE discover
-./psl-cmd.py --interface $INTERFACE query --mac $MAC all
-./psl-cmd.py --interface $INTERFACE set --mac $MAC --passwd $PW --name $NAME
-./psl-cmd.py --interface $INTERFACE discover |grep $NAME
+./psl.py --interface $INTERFACE discover
+./psl.py --interface $INTERFACE query --mac $MAC all
+./psl.py --interface $INTERFACE set --mac $MAC --passwd $PW --name $NAME
+./psl.py --interface $INTERFACE discover |grep $NAME
 if [ "$?" != "0" ] ; then 
     echo "Name not set!"
 fi
-./psl-cmd.py --interface $INTERFACE set --mac $MAC --passwd $PW --dhcp off --ip 192.168.11.117 --netmask 255.255.255.0 --gateway 192.168.11.2
-./psl-cmd.py --interface $INTERFACE query --mac $MAC dhcp ip gateway netmask
-./psl-cmd.py --interface $INTERFACE set --mac $MAC --passwd $PW --dhcp off --ip 192.168.11.116 --netmask 255.255.255.0 --gateway 192.168.11.1
-./psl-cmd.py --interface $INTERFACE query --mac $MAC dhcp ip gateway netmask
-./psl-cmd.py --interface $INTERFACE set --mac $MAC --passwd $PW --dhcp on
-./psl-cmd.py --interface $INTERFACE query --mac $MAC dhcp ip gateway netmask
+./psl.py --interface $INTERFACE set --mac $MAC --passwd $PW --dhcp off --ip 192.168.11.117 --netmask 255.255.255.0 --gateway 192.168.11.2
+./psl.py --interface $INTERFACE query --mac $MAC dhcp ip gateway netmask
+./psl.py --interface $INTERFACE set --mac $MAC --passwd $PW --dhcp off --ip 192.168.11.116 --netmask 255.255.255.0 --gateway 192.168.11.1
+./psl.py --interface $INTERFACE query --mac $MAC dhcp ip gateway netmask
+./psl.py --interface $INTERFACE set --mac $MAC --passwd $PW --dhcp on
+./psl.py --interface $INTERFACE query --mac $MAC dhcp ip gateway netmask
