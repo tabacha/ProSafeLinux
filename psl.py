1b66084a38617b8144be7f1537f21360abb895bf
Sven Anders <github-commit@sven.anders.im>
2012-06-19 15:19:02 +0200

    README.md

--- psl.py
+++ psl.py
@@ -1,347 +1,180 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import time
-import binascii
-import pprint
-import struct
-import random
-import socket
-import fcntl
+"Main Programm executed by the user"
+
+import argparse
+import sys
+from psl_class import ProSafeLinux
 import psl_typ
-import inspect
 
 
-def get_hw_addr(ifname):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    info = fcntl.ioctl(sock.fileno(), 0x8927, struct.pack('256s', ifname[:15]))
-    return ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1]
+def discover(args, switch):
+    "Search for Switches"
+    print "Searching for ProSafe Plus Switches ...\n"
+    switch.discover()
 
 
-def get_ip_address(ifname):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # 0x8915 = SIOCGIFADDR
-    return socket.inet_ntoa(fcntl.ioctl(sock.fileno(), 0x8915,
-                                         struct.pack('256s',
-                                                     ifname[:15]))[20:24])
+def set_switch(args, switch):
+    "Set values in one switch"
+    cmds = {ProSafeLinux.CMD_PASSWORD: args.passwd[0]}
+    for scmd in switch.get_setable_cmds():
+        if vars(args)[scmd.get_name()] is not None:
+            if isinstance(scmd, psl_typ.PslTypAction):
+                if vars(args)[scmd.get_name()]:
+                    cmds[scmd] = True
+            else:
+                if isinstance(scmd, psl_typ.PslTypBoolean):
+                    cmds[scmd] = (vars(args)[scmd.get_name()][0] == "on")
+                else:
+                    cmds[scmd] = vars(args)[scmd.get_name()][0]
+
+    if ProSafeLinux.CMD_DHCP in cmds:
+        if cmds[ProSafeLinux.CMD_DHCP]:
+            if ((ProSafeLinux.CMD_IP in cmds) or
+                (ProSafeLinux.CMD_GATEWAY in cmds) or
+                (ProSafeLinux.CMD_NETMASK in cmds)):
+                print "When dhcp=on, no ip,gateway nor netmask is allowed"
+                return
+        else:
+            if (not((ProSafeLinux.CMD_IP in cmds) and
+              (ProSafeLinux.CMD_GATEWAY in cmds) and
+              (ProSafeLinux.CMD_NETMASK in cmds))):
+                print "When dhcp=off, specify ip,gateway and netmask"
+                return
+    else:
+        if ((ProSafeLinux.CMD_IP in cmds) or
+          (ProSafeLinux.CMD_GATEWAY in cmds) or
+          (ProSafeLinux.CMD_NETMASK in cmds)):
+            print "Use dhcp on,ip,gateway and netmask option together"
+            return
+
+    print "Changing Values..\n"
+    switch.transmit(cmds, args.mac[0], switch.transfunc)
 
 
-def pack_mac(value):
-    if (len(value) == 17):
-        return binascii.unhexlify(value[0:2] + value[3:5] + value[6:8] +
-                                  value[9:11] + value[12:14] + value[15:17])
-    if (len(value) == 12):
-        return binascii.unhexlify(value)
-    raise "unkown mac format=" + value
+def query(args, switch):
+    "query values form the switch"
+    print "Query Values..\n"
+    if not(args.passwd == None):
+        login = {switch.CMD_PASSWORD: args.passwd[0]}
+        switch.transmit(login, args.mac[0], switch.transfunc)
+    query_cmd = []
+    for qarg in args.query:
+        if qarg == "all":
+            for k in switch.get_query_cmds():
+                if ((k != ProSafeLinux.CMD_VLAN_ID) and
+                    (k != ProSafeLinux.CMD_VLAN802_ID)):
+                    query_cmd.append(k)
+        else:
+            query_cmd.append(switch.get_cmd_by_name(qarg))
+    switch.query(query_cmd, args.mac[0], switch.storefunc)
+    for key in switch.outdata.keys():
+        if isinstance(key, psl_typ.PslTyp):
+            key.print_result(switch.outdata[key])
+        else:
+            if args.debug:
+                print "-%-29s%s" % (key, switch. outdata[key])
 
 
-def unpack_mac(value):
-    mac = binascii.hexlify(value)
-    return (mac[0:2] + ":" + mac[2:4] + ":" + mac[4:6] + ":" + mac[6:8] +
-            ":" + mac[8:10] + ":" + mac[10:12])
-
-
-class ProSafeLinux:
-    CMD_MODEL = psl_typ.PslTypString(0x0001, "model")
-    CMD_FIMXE2 = psl_typ.PslTypHex(0x0002, "fixme2")
-    CMD_NAME = psl_typ.PslTypString(0x0003, "name")
-    CMD_MAC = psl_typ.PslTypMac(0x0004, "MAC")
-    CMD_FIMXE5 = psl_typ.PslTypHex(0x0005, "fixme5")
-    CMD_IP = psl_typ.PslTypIpv4(0x0006, "ip")
-    CMD_NETMASK = psl_typ.PslTypIpv4(0x0007, "netmask")
-    CMD_GATEWAY = psl_typ.PslTypIpv4(0x0008, "gateway")
-    CMD_NEW_PASSWORD = psl_typ.PslTypPassword(0x0009, "new_password", True)
-    CMD_PASSWORD = psl_typ.PslTypPassword(0x000a, "password", False)
-    CMD_DHCP = psl_typ.PslTypDHCP(0x000b, "dhcp")
-    CMD_FIXMEC = psl_typ.PslTypHex(0x000c, "fixmeC")
-    CMD_FIRMWAREV = psl_typ.PslTypStringQueryOnly(0x000d, "firmwarever")
-    CMD_FIMXEE = psl_typ.PslTypHex(0x000e, "fixmeE")
-    CMD_FIXMEF = psl_typ.PslTypHex(0x000f, "fixmeF")
-    CMD_REBOOT = psl_typ.PslTypAction(0x0013, "reboot")
-    CMD_FACTORY_RESET = psl_typ.PslTypAction(0x0400, "factory_reset")
-    CMD_SPEED_STAT = psl_typ.PslTypSpeedStat(0x0c00, "speed_stat")
-    CMD_PORT_STAT = psl_typ.PslTypPortStat(0x1000, "port_stat")
-    CMD_RESET_PORT_STAT = psl_typ.PslTypAction(0x1400, "reset_port_stat")
-    CMD_TEST_CABLE = psl_typ.PslTypHexNoQuery(0x1800, "test_cable")
-    CMD_TEST_CABLE_RESP = psl_typ.PslTypHexNoQuery(0x1c00, "test_cable_resp")
-    CMD_VLAN_SUPP = psl_typ.PslTypHex(0x2000, "vlan_supp")
-    CMD_VLAN_ID = psl_typ.PslTypVlanId(0x2400, "vlan_id")
-    CMD_VLAN802_ID = psl_typ.PslTypHex(0x2800, "vlan802_id")
-    CMD_VLANPVID = psl_typ.PslTypVlanPVID(0x3000, "vlan_pvid")
-    CMD_QUALITY_OF_SERVICE = psl_typ.PslTypHex(0x3400, "qos")
-    CMD_PORT_BASED_QOS = psl_typ.PslTypPortBasedQOS(0x3800, "port_bases_qos")
-    CMD_BANDWITH_INCOMMING_LIMIT = psl_typ.PslTypBandwith(
-                                              0x4c00, "bandwith_in")
-    CMD_BANDWITH_OUTGOING_LIMIT = psl_typ.PslTypBandwith(
-                                              0x5000, "bandwith_out")
-    CMD_FIXME5400 = psl_typ.PslTypHex(0x5400, "fxime5400")
-    CMD_BROADCAST_FILTER = psl_typ.PslTypBroadcastFilter(0x5800,
-                 "broadcast_filter")
-    CMD_PORT_MIRROR = psl_typ.PslTypHex(0x5c00, "port_mirror")
-    CMD_NUMBER_OF_PORTS = psl_typ.PslTypHex(0x6000, "number_of_ports")
-    CMD_IGMP_SNOOPING = psl_typ.PslTypIGMPSnooping(0x6800, "igmp_snooping")
-    CMD_BLOCK_UNKOWN_MULTICAST = psl_typ.PslTypBoolean(
-                                              0x6c00, "block_unknown_multicast")
-    CMD_IGMP_HEADER_VALIDATION = psl_typ.PslTypBoolean(0x7000,
-        "igmp_header_validation")
-    CMD_FIXME7400 = psl_typ.PslTypHex(0x7400, "fixme7400")
-    CMD_END = psl_typ.PslTypEnd(0xffff, "END")
-
-    CTYPE_QUERY_REQUEST = 0x0101
-    CTYPE_QUERY_RESPONSE = 0x0102
-    CTYPE_TRANSMIT_REQUEST = 0x103
-    CTYPE_TRANSMIT_RESPONSE = 0x104
-
-    FLAG_PASSWORD_ERROR = 0x000a
-
-    RECPORT = 63321
-    SENDPORT = 63322
-
-    def __init__(self):
-        self.myhost = None
-        self.srcmac = None
-        self.ssocket = None
-        self.rsocket = None
-
-        self.seq = random.randint(100, 2000)
-        self.outdata = {}
-        self.debug = False
-        self.mac_cache = {}
-        self.cmd_by_id = {}
-        self.cmd_by_name = {}
-        for key, value in  inspect.getmembers(ProSafeLinux):
-            if key.startswith("CMD_"):
-                self.cmd_by_name[value.get_name()] = value
-                self.cmd_by_id[value.get_id()] = value
-
-    def bind(self, interface):
-        self.myhost = get_ip_address(interface)
-        self.srcmac = pack_mac(get_hw_addr(interface))
-
-            # send socket
-        self.ssocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.ssocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.ssocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
-        # The following requires root permission so we do not do this:
-        # self.socket.setsockopt(socket.SOL_SOCKET,
-        #                               IN.SO_BINDTODEVICE,"eth1"+'\0')
-
-        self.ssocket.bind((self.myhost, self.RECPORT))
-
-        # recive socket
-        self.rsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.rsocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.rsocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self.rsocket.bind(("255.255.255.255", self.RECPORT))
-
-    def get_query_cmds(self):
-        rtn = []
-        for cmd in self.cmd_by_name.values():
-            if cmd.is_queryable():
-                rtn.append(cmd)
-        return rtn
-
-    def get_setable_cmds(self):
-        rtn = []
-        for cmd in self.cmd_by_name.values():
-            if cmd.is_setable():
-                rtn.append(cmd)
-        return rtn
-
-    def get_cmd_by_name(self, name):
-        return self.cmd_by_name[name]
-
-    def set_debug_output(self):
-        self.debug = True
-
-    def recv(self, recvfunc, maxlen=8192, timeout=0.005):
-        self.rsocket.settimeout(timeout)
+def query_raw(args, switch):
+    "get all values, even unkown"
+    print "QUERY DEBUG RAW"
+    if not(args.passwd == None):
+        login = {switch.CMD_PASSWORD: args.passwd[0]}
+        switch.transmit(login, args.mac[0], switch.transfunc)
+    i = 0x0001
+    while (i < ProSafeLinux.CMD_END.get_id()):
+        query_cmd = []
+        query_cmd.append(psl_typ.PslTypHex(i, "Command %d" % i))
         try:
-            message, address = self.rsocket.recvfrom(maxlen)
-        except socket.timeout:
-            return None
-        if self.debug:
-            print "recv=" + binascii.hexlify(message)
-        if recvfunc is not None:
-            recvfunc(message, address)
-        self.recv(recvfunc, maxlen, timeout)
+            switch.query(query_cmd, args.mac[0], switch.rec_raw)
+            found = None
+            for qcmd in switch.outdata.keys():
+                if (isinstance(qcmd, psl_typ.PslTyp)):
+                    if qcmd.get_id() == i:
+                        found = qcmd
 
-    def parse_packet(self, pack, unknown_warn):
-        data = {}
-        data["seq"] = struct.unpack(">H", pack[22:24])[0]
-        data["ctype"] = struct.unpack(">H", pack[0:2])[0]
-        data["flags"] = struct.unpack(">H", pack[4:6])[0]
-        data["mymac"] = binascii.hexlify(pack[8:14])
-        data["theirmac"] = binascii.hexlify(pack[14:20])
-        pos = 32
-        cmd_id = 0
-        while(cmd_id != self.CMD_END.get_id()):
-            cmd_id = struct.unpack(">H", pack[pos:(pos + 2)])[0]
-            if cmd_id in self.cmd_by_id:
-                cmd = self.cmd_by_id[cmd_id]
+            if found is None:
+                print "NON:%04x:%-29s:%s" % (i, "", switch.outdata["raw"])
             else:
-                if unknown_warn:
-                    print "Unkown Response %d" % cmd_id
-                cmd = psl_typ.PslTypHex(cmd_id, "UNKOWN %d" % cmd_id)
-            pos = pos + 2
-            leng = struct.unpack(">H", pack[pos:(pos + 2)])[0]
-            pos = pos + 2
-            if leng > 0:
-                value = cmd.unpack_py(pack[pos:(pos + leng)])
-            else:
-                value = None
-            if cmd in data:
-                if type(data[cmd]) != type(list()):
-                    data[cmd] = [data[cmd]]
-                data[cmd].append(value)
-            else:
-                data[cmd] = value
-            if self.debug:
-                print "cmd=", cmd_id, " len=", leng, " data=", binascii.hexlify(
-                    pack[pos:(pos + leng)])
-            pos = pos + leng
-        #pprint.pprint(data)
-        return data
-
-    def discoverfunc(self, msg, adr):
-        #print "==FOUND SWITCH=="
-        data = self.parse_packet(msg, True)
-        dhcpstr = ""
-        if (data[self.CMD_DHCP]):
-            dhcpstr = " DHCP=on"
-        print " * %s\t%s\t%s\t%s\t%s" % (data[self.CMD_MAC],
-                                         data[self.CMD_IP],
-                                         data[self.CMD_MODEL],
-                                         data[self.CMD_NAME],
-                                         dhcpstr)
-
-    def storediscoverfunc(self, msg, adr):
-        #print "==FOUND SWITCH=="
-        data = self.parse_packet(msg, True)
-        if self.debug:
-            print "Store MAC, IP: " + (data[self.CMD_MAC] + " " +
-               data[self.CMD_IP])
-        self.mac_cache[data[self.CMD_MAC]] = data[self.CMD_IP]
-        #print " * %s\t%s\t%s\t%s\t%s" % (data[self.CMD_MAC],
-        # data[self.CMD_IP], data[self.CMD_MODEL], data[self.CMD_NAME], dhcpstr)
-
-    def transfunc(self, msg, adr):
-        #print "==FOUND SWITCH=="
-        data = self.parse_packet(msg, True)
-        if self.debug:
-            pprint.pprint(data)
-            if data["flags"] == self.FLAG_PASSWORD_ERROR:
-                print "wrong password"
-            if data["flags"] == 0:
-                print "success"
-
-    def storefunc(self, msg, adr):
-        #print "==FOUND SWITCH=="
-        self.outdata = self.parse_packet(msg, True)
-        if self.debug:
-            pprint.pprint(self.outdata)
-
-            if self.outdata["flags"] == self.FLAG_PASSWORD_ERROR:
-                print "Flags: wrong password"
-
-            if self.outdata["flags"] == 0:
-                print "Flags: success"
-
-    def rec_raw(self, msg, adr):
-        try:
-            self.outdata = self.parse_packet(msg, False)
+                print "RES:%04x:%-29s:%s " % (i, switch.outdata[found],
+                    switch.outdata["raw"])
+            if args.debug:
+                for key in switch.outdata.keys():
+                    print "%x-%-29s%s" % (i, key, switch.outdata[key])
+        except (KeyboardInterrupt, SystemExit):
+            raise
         except:
-            pass
-        self.outdata["raw"] = binascii.hexlify(msg)
+            print "ERR:%04x:%s" % (i, sys.exc_info()[1])
+        i = i + 1
 
-    def send(self, host, port, data):
-        if self.debug:
-            print "send to ip " + host + " data = " + binascii.hexlify(data)
-        self.ssocket.sendto(data, (host, port))
-        self.seq += 1
 
-    def baseudp(self, ctype, destmac):
-        reserved = "\x00"
-        if destmac is None:
-            destmac = 6 * "\x00"
-        if len(destmac) > 6:
-            destmac = pack_mac(destmac)
-        data = (struct.pack(">h", ctype) + 6 * reserved + self.srcmac +
-                     destmac + 2 * reserved)
-        data += struct.pack(">h", self.seq)
-        data += "NSDP" + 4 * reserved
-        return data
+def main():
+    "main program"
+    cmd_funcs = {
+        "discover": discover,
+        "set": set_switch,
+        "query": query,
+        "query_raw": query_raw,
+    }
 
-    @staticmethod
-    def addudp(cmd, datain=None):
-        data = struct.pack(">H", cmd.get_id())
-        if (datain is None):
-            data += struct.pack(">H", 0)
+    switch = ProSafeLinux()
+    parser = argparse.ArgumentParser(
+        description='Manage Netgear ProSafe Plus switches under linux.')
+    parser.add_argument("--interface", nargs=1, help="Interface",
+        default=["eth0"])
+    parser.add_argument("--debug", help="Debug output", action='store_true')
+    subparsers = parser.add_subparsers(help='operation', dest="operation")
+
+    subparsers.add_parser('discover', help='Find all switches in all subnets')
+
+    query_parser = subparsers.add_parser("query",
+        help="Query values from the switch")
+    query_parser.add_argument("--mac", nargs=1,
+        help="Hardware adresse of the switch", required=True)
+    query_parser.add_argument("--passwd", nargs=1, help="password")
+    choices = []
+    for cmd in switch.get_query_cmds():
+        choices.append(cmd.get_name())
+    choices.append("all")
+    query_parser.add_argument("query", nargs="+", help="What to query for",
+        choices=choices)
+
+    query_parser = subparsers.add_parser("query_raw",
+        help="Query raw values from the switch")
+    query_parser.add_argument("--mac", nargs=1,
+        help="Hardware adresse of the switch", required=True)
+    query_parser.add_argument("--passwd", nargs=1,
+        help="password")
+
+    set_parser = subparsers.add_parser("set", help="Set values to the switch")
+    set_parser.add_argument("--mac", nargs=1,
+        help="Hardware adresse of the switch", required=True)
+    set_parser.add_argument("--passwd", nargs=1, help="password", required=True)
+
+    for cmd in switch.get_setable_cmds():
+        if isinstance(cmd, psl_typ.PslTypAction):
+            set_parser.add_argument("--" + cmd.get_name(),
+                dest=cmd.get_name(), action='store_true')
+
         else:
-            pdata = cmd.pack_py(datain)
-            data += struct.pack(">H", len(pdata))
-            data += pdata
-        return data
+            if isinstance(cmd, psl_typ.PslTypBoolean):
+                set_parser.add_argument("--" + cmd.get_name(), nargs=1,
+                    choices=["on", "off"])
+            else:
+                set_parser.add_argument("--" + cmd.get_name(), nargs=1)
 
-    def ip_from_mac(self, mac):
-        if mac is None:
-            return "255.255.255.255"
-        if mac in self.mac_cache:
-            return self.mac_cache[mac]
-        #print "mac="+mac
-        # FIXME: Search in /proc/net/arp if mac there use this one
-        #with open("/proc/net/arp") as f:
-        # for line in f:
-        #   print line
-        query_arr = [self.CMD_MAC, self.CMD_IP]
-        self.query(query_arr, mac, self.storediscoverfunc, use_ip_func=False)
-        if mac in self.mac_cache:
-            return self.mac_cache[mac]
-        print "cant find mac: " + mac
-        return "255.255.255.255"
+    args = parser.parse_args()
+    interface = args.interface[0]
 
-    def query(self, cmd_arr, mac, func, use_ip_func=True):
-        if use_ip_func:
-            ipadr = self.ip_from_mac(mac)
-        else:
-            ipadr = "255.255.255.255"
-        data = self.baseudp(destmac=mac, ctype=self.CTYPE_QUERY_REQUEST)
-        for cmd in cmd_arr:
-            data += self.addudp(cmd)
-        data += self.addudp(self.CMD_END)
-        self.outdata = {}
-        self.send(ipadr, self.SENDPORT, data)
-        time.sleep(0.7)
-        self.recv(func)
+    switch.bind(interface)
 
-    def transmit(self, cmd_arr, mac, func):
-        ipadr = self.ip_from_mac(mac)
-        data = self.baseudp(destmac=mac, ctype=self.CTYPE_TRANSMIT_REQUEST)
-        if self.CMD_PASSWORD in cmd_arr:
-            data += self.addudp(self.CMD_PASSWORD, cmd_arr[self.CMD_PASSWORD])
-        for cmd, pdata in cmd_arr.items():
-            if cmd != self.CMD_PASSWORD:
-                data += self.addudp(cmd, pdata)
-        data += self.addudp(self.CMD_END)
-        self.send(ipadr, self.SENDPORT, data)
-        time.sleep(0.7)
-        self.recv(func)
+    if (args.debug):
+        switch.set_debug_output()
 
-    def passwd(self, mac, old, new, func):
-        # The Order of the CMD_PASSWORD and CMD_NEW_PASSWORD is important
-        ipadr = self.ip_from_mac(mac)
-        data = self.baseudp(destmac=mac, ctype=self.CTYPE_TRANSMIT_REQUEST)
-        data += self.addudp(self.CMD_PASSWORD, old)
-        data += self.addudp(self.CMD_NEW_PASSWORD, new)
-        data += self.addudp(self.CMD_END)
-        self.send(ipadr, self.SENDPORT, data)
-        time.sleep(0.7)
-        self.recv(func)
+    if args.operation in cmd_funcs:
+        cmd_funcs[args.operation](args, switch)
+    else:
+        print "ERROR: operation not found!"
 
-    def discover(self):
-        query_arr = [self.CMD_MODEL,
-                   self.CMD_NAME,
-                   self.CMD_MAC,
-                   self.CMD_DHCP,
-                   self.CMD_IP]
-        self.query(query_arr, None, self.discoverfunc)
+main()
